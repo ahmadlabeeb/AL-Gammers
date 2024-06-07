c4a61c6638b519716fc91f283c16d83d79f4f660
alabeeb <alabeeb81@gmail.com>
2024-06-07 15:32:32 +0300

    Initial Commit

---
+++ Gammers/GammersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GammersApp.swift
+//  Gammers
+//
+//  Created by Ahmad Labeeb on 07/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct GammersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
